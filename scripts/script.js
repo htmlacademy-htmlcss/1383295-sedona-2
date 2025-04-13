ef2520548033bd3a741dbda92a9803ee2d691ba8
Rakhim92 <61398357+Rakhim92@users.noreply.github.com>
2025-04-14 02:29:31 +0700

    По мелочам

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,33 @@
+// Скрытие лейбла внутри инпута при фокусе на поле ввода
 
+const subscriptionInput = document.querySelector(".subscribing-input")
+const label = document.querySelector(".subscribing-control")
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  function toggleLabel() {
+
+
+    function hideLabel() {
+      label.classList.add('visually-hidden')
+    }
+
+    function showLabel() {
+      label.classList.remove('visually-hidden')
+    }
+
+    subscriptionInput.addEventListener('focusin', hideLabel)
+    subscriptionInput.addEventListener('focusout', () => {
+      if (subscriptionInput.value.length > 0) return
+      showLabel()
+    })
+  }
+
+  toggleLabel()
+
+})
+
+// Модальное окно
 const modalMenu = document.querySelector(".modal-container");
 const closeModal = document.querySelector(".modal-close-button");
 const searchButton = document.querySelector(".search-button");
